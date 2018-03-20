84333ccfd1a26f163eb8cc017de111cf163c29b7
jiaeyan <svenyan234@gmail.com>
2018-03-20 02:15:47 -0400

    change

--- run.py
+++ run.py
@@ -13,7 +13,7 @@ def generate_feature_file(pairs, out_f, train=True):
 def main():
     parser = argparse.ArgumentParser(description='Run relation extraction')
     parser.add_argument('--gold', type=str, help='the input gold file to generate train feature file')
-    parser.add_argument('--train', type=bool, help='decide if this procedure is train or not')
+    parser.add_argument('--train', type=bool, default=False, help='decide if this procedure is train or not')
     args = parser.parse_args()
 
     if args.train:
@@ -21,8 +21,10 @@ def main():
     else:
         pairs = get_pairs("data/rel-testset.gold")
     if args.train:
+        print("yes trianing")
         generate_feature_file(pairs, "train.txt", train=args.train)
     else:
+        print("yes testing")
         generate_feature_file(pairs, "test.txt", train=args.train)
 
 
